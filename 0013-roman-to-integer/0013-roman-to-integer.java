5266f26519dd5916152db170f1334379502c9e4a
Chaitanya Kurwade <106234527+chaitanya-kurwade@users.noreply.github.com>
2025-06-09 23:36:28 +0530

    Time: 2 ms (100%), Space: 45 MB (24.4%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,42 +1,40 @@
 class Solution {
     public int romanToInt(String s) {
-        int result = 0;
-
-        if (s.contains("IV")) {
-            result += 4;
-            s = s.replaceFirst("IV", "");
+        int num = 0;
+        int prev = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char ch = s.charAt(i);
+            int curr = 0;
+            switch (ch) {
+                case 'I':
+                    curr = 1;
+                    break;
+                case 'V':
+                    curr = 5;
+                    break;
+                case 'X':
+                    curr = 10;
+                    break;
+                case 'L':
+                    curr = 50;
+                    break;
+                case 'C':
+                    curr = 100;
+                    break;
+                case 'D':
+                    curr = 500;
+                    break;
+                case 'M':
+                    curr = 1000;
+                    break;
+            }
+            if (curr < prev) {
+                num -= curr;
+            } else {
+                num += curr;
+            }
+            prev = curr;
         }
-        if (s.contains("IX")) {
-            result += 9;
-            s = s.replaceFirst("IX", "");
-        }
-        if (s.contains("XL")) {
-            result += 40;
-            s = s.replaceFirst("XL", "");
-        }
-        if (s.contains("XC")) {
-            result += 90;
-            s = s.replaceFirst("XC", "");
-        }
-        if (s.contains("CD")) {
-            result += 400;
-            s = s.replaceFirst("CD", "");
-        }
-        if (s.contains("CM")) {
-            result += 900;
-            s = s.replaceFirst("CM", "");
-        }
-
-        for (char c : s.toCharArray()) {
-            if (c == 'I') result += 1;
-            else if (c == 'V') result += 5;
-            else if (c == 'X') result += 10;
-            else if (c == 'L') result += 50;
-            else if (c == 'C') result += 100;
-            else if (c == 'D') result += 500;
-            else if (c == 'M') result += 1000;
-        }
-
-        return result;
+        return num;
     }
 }
