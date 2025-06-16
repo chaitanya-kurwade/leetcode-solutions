ac49b987c5ba6210621b288fb05d84d010f47c93
Chaitanya Kurwade <106234527+chaitanya-kurwade@users.noreply.github.com>
2025-06-16 23:49:41 +0530

    Time: 0 ms (100%), Space: 40.2 MB (80.29%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,6 +1,19 @@
 class Solution {
     public int fib(int n) {
-        if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int first = 0;
+        int second = 1;
+        int third = 0;
+        for (int i = 2; i <= n; i++) {
+            third = first + second;
+            first = second;
+            second = third;
+        }
+        return third;
     }
 }
