bbdbad258bdc8db1661f54fd2292d5d25aba1e5e
Chaitanya Kurwade <106234527+chaitanya-kurwade@users.noreply.github.com>
2025-06-16 23:33:34 +0530

    Time: 0 ms (100%), Space: 45.7 MB (90.78%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public void reverseString(char[] s) {
-        int left = 0, right = s.length - 1;
-        while (right > left) {
+        int left = 0, right = s.length-1;
+        while(left<right) {
             char temp = s[left];
             s[left] = s[right];
             s[right] = temp;
